4cb901edfdc110b2b4c6dcc158725f677ab6e921
CodeFace <icodeface@users.noreply.github.com>
2023-11-08 15:34:43 +0800

    fix typo

--- pages/anniversary.vue
+++ pages/anniversary.vue
@@ -472,7 +472,7 @@ import imgs from '@/constant/partner-item-img'
 export default {
   head() {
     return {
-      title: 'FoxWallet 2st Anniversary'
+      title: 'FoxWallet 2nd Anniversary'
     }
   },
   data() {
