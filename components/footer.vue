3f85473fbc27f9dca4092748840663b41c591042
monster1935 <monster.1935.li@gmail.com>
2024-02-01 11:15:49 +0800

    feat: add open source url in footer  (#9)
    * feat: add chrome store download link
    
    * feat: replace real chrome app store url
    
    * fix: fix redirect url
    
    * feat: add open source url
    
    ---------
    
    Co-authored-by: monster1935 <monster1935@web3.com>

--- components/footer.vue
+++ components/footer.vue
@@ -20,12 +20,12 @@
             <h3 class="pt-2 pb-4 font-bold text-xl">
               {{ $t('nav.learnMore') }}
             </h3>
-            <el-tooltip class="item" effect="dark" :content="$t('nav.earn.coming')" placement="left">
+            <!-- <el-tooltip class="item" effect="dark" :content="$t('nav.earn.coming')" placement="left">
               <span class="cursor-pointer hover:opacity-75 text-gray-700 mb-2">{{ $t("nav.earn.title") }}</span>
-            </el-tooltip>
-            <!-- <a class="cursor-pointer hover:opacity-75 text-gray-700 mb-2" target="_blank" href="">
-              {{ $t('nav.connect') }}
-            </a> -->
+            </el-tooltip> -->
+            <a class="cursor-pointer hover:opacity-75 text-gray-700 mb-2" target="_blank" href="https://github.com/foxwallet">
+              {{ $t('nav.openSource') }}
+            </a>
             <!-- <a class="cursor-pointer hover:opacity-75 text-gray-700 mb-2" target="_blank" href="">
               {{ $t('nav.staking') }}
             </a> -->
