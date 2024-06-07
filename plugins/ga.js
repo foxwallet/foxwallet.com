7b52c8b2f17166e2140cdcfaf1beeccc85caf4b4
monster1935 <monster.1935.li@gmail.com>
2024-06-07 14:44:15 +0800

    fix: try to fix data report

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,15 +1,16 @@
 if (process.client && process.env.NODE_ENV === 'production') {
+  console.log('Data connected!')
   window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
+  function gtag() { dataLayer.push(arguments); }
   gtag('js', new Date());
-  gtag('config', '');
+  gtag('config', 'G-ST120Y7Q6Y');
 }
 
 export default ({ app: { router }, store }) => {
- if (process.client && process.env.NODE_ENV === 'production') {
-  router.afterEach((to, from) => {
-    ga('set', 'page', to.fullPath)
-    ga('send', 'pageview')
-  })
-}
+  if (process.client && process.env.NODE_ENV === 'production') {
+    router.afterEach((to, from) => {
+      ga('set', 'page', to.fullPath)
+      ga('send', 'pageview')
+    })
+  }
 }
