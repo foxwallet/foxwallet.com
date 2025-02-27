c96826d481ffeade4dce0f3b6c4d2f9ddc5ef2a2
monster1935 <monster.1935.li@gmail.com>
2025-02-27 17:26:18 +0800

    feat: replace docs site url in footer

--- components/new-footer.vue
+++ components/new-footer.vue
@@ -20,28 +20,28 @@
               <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://github.com/foxwallet/security-audit-certification">
                 {{ $t('nav.auditReport') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/resource">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/resource`">
                 {{ $t('nav.brandResource') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/official-links">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/official-links`">
                 {{ $t('nav.offcialLinks') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/developer/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/developer/`">
                 {{ $t('nav.developer') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/faq">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/faq`">
                 {{ $t('nav.faqs') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/fox-credit">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/fox-credit`">
                 {{ $t('nav.foxCredit') }}
               </a>
               <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://foxwallet.com/download">
                 {{ $t('nav.download.title') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/privacy-policy">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/privacy-policy`">
                 {{ $t('nav.privacy') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/terms-of-service">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/terms-of-service`">
                 {{ $t('nav.terms') }}
               </a>
             </div>
@@ -51,40 +51,40 @@
               {{ $t('footer.supportedNetworks') }}
             </div>
             <div class="flex flex-col">
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/bitcoin/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/bitcoin/`">
                 Bitcoin {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/ethereum/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/ethereum/`">
                 Ethereum {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/aleo/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/aleo/`">
                 Aleo {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/aptos/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/aptos/`">
                 Aptos {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/cosmos/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/cosmos/`">
                 Cosmos {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/filecoin/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/filecoin/`">
                 Filecoin {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/ironfish/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/ironfish/`">
                 IronFish {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/qtum/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/qtum/`">
                 Qtum {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/sui/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/sui/`">
                 Sui {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/spacemesh/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/spacemesh/`">
                 Spacemesh {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/ton/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/ton/`">
                 Ton {{ $t('main.wallet') }}
               </a>
-              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/tron/">
+              <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/tron/`">
                 Tron {{ $t('main.wallet') }}
               </a>
             </div>
@@ -123,7 +123,7 @@
           <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://github.com/foxwallet/security-audit-certification">
             {{ $t('nav.auditReport') }}
           </a>
-          <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/resource">
+          <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/resource`">
             {{ $t('nav.brandResource') }}
           </a>
         </div>
@@ -133,13 +133,13 @@
           {{ $t('nav.about.title') }}
         </div>
         <div class="flex flex-col">
-          <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com">
+          <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}`">
             {{ $t('nav.docs.title') }}
           </a>
-          <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/faq">
+          <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/faq`">
             {{ $t('nav.about.faq') }}
           </a>
-          <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" href="https://hc.foxwallet.com/docs/developer/">
+          <a class="cursor-pointer opacity-60 hover:opacity-75 text-white mb-3" target="_blank" :href="`${baseUrl}/docs/developer/`">
             {{ $t('nav.developer') }}
           </a>
         </div>
@@ -160,6 +160,22 @@
     </div>
   </div>
 </template>
+
+<script>
+
+export default {
+  name: 'NewFooter',
+  computed: {
+    baseUrl() {
+      if (this.$i18n.locale === 'zh') {
+        return 'https://hc.foxwallet.com/zh'
+      }
+      return 'https://hc.foxwallet.com'
+    }
+  }
+}
+
+</script>
 
 <style scoped>
   .content-wrapper {
